dd01b38697e494325fb3d1a1a579244534a0d884
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-18 20:10:06 +0430

    set up questiona

--- src/App.js
+++ src/App.js
@@ -15,8 +15,28 @@ function App() {
     return <Loading />
   }
 
+  console.log(questions[0]);
+  const { question, incorrect_answers, correct_answer } = questions[0]
+  const answer = { ...incorrect_answers, correct_answer }
+
   return (
-    <main>quiz app</main>
+    <main><Modal />
+      <section className="quiz">
+        <p className="correct-answer">
+          correct answers :{correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {correct.map((answer, index) => {
+              return (
+                <button className="answer-btn" key={index} dangerouslySetInnerHTML={{ __html: answer }} />
+              )
+            })}
+          </div>
+        </article>
+      </section>
+    </main>
   )
 }
 
