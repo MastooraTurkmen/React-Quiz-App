cb43f2d5a4af3b9db8f8a0ae5affaba6e4f53ae3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-19 07:56:19 +0430

    set up close modal

--- src/Modal.js
+++ src/Modal.js
@@ -8,6 +8,10 @@ const Modal = () => {
     <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
       <div className="modal-content">
         <h2>congrats!</h2>
+        <p>You answered of questions correctly</p>
+        <button className="close-btn" onClick={closeModal}>
+          play again
+        </button>
       </div>
     </div>
   )
