b6d14a447a5e9c35e8152d90af8ac4395039f32f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-19 07:58:17 +0430

    correct answers

--- src/Modal.js
+++ src/Modal.js
@@ -8,7 +8,7 @@ const Modal = () => {
     <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
       <div className="modal-content">
         <h2>congrats!</h2>
-        <p>You answered of questions correctly</p>
+        <p>You answered {((correct / questions.length) * 100).toFixed(0)}% of questions correctly</p>
         <button className="close-btn" onClick={closeModal}>
           play again
         </button>
